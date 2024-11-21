a3934e53501da9c2ae8717a7c285ee41338d375d
Behannel <mosquitoestudante34234@gmail.com>
2024-11-21 09:39:11 -0300

    Implementação do segredo numero 1

---
+++ Halloween Xande SURP/Assets/_Scripts/ScriptsSol/AnimEvents.cs
@@ -0,0 +1,14 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimEvents : MonoBehaviour
+{
+
+    public void EndAnim()
+    {
+        
+    }
+
+}
--- Halloween Xande SURP/Assets/_Scripts/ScriptsSol/DialogueTrigger.cs
+++ Halloween Xande SURP/Assets/_Scripts/ScriptsSol/DialogueTrigger.cs
@@ -4,15 +4,26 @@ using UnityEngine.Events;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+    public GameObject InteractWithE;
     public UnityEvent startnextSentence;
 
     public UnityEvent OnDialogueFinish;
 
-    private void OnMouseOver()
+
+    private void OnTriggerStay2D(Collider2D col)
     {
-        TriggerDialogue();
+        if (col.CompareTag("Player"))
+        {
+            TriggerDialogue();
+            InteractWithE.SetActive(true);
+        }
+        
     }
 
+    private void OnTriggerExit2D()
+    {
+        InteractWithE.SetActive(false);
+    }
     public void TriggerDialogue()
     {
         if (Input.GetKeyDown(KeyCode.E))
